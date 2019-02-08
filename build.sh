ce993f82a2ed34c4d113b6bdcd8d98383984ecf7
Andres Arias <mrlocke@users.noreply.github.com>
2019-02-08 11:34:33 +0100

    Conditional build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 for (( c=1; c<=5; c++ ))
 do  
-   echo "Welcome $c to release the Jenkins class"
+   echo "$c - Welcome $name to release $BUILD_NUMBER of the Jenkins class"
 done
-echo Build Number - $BUILD_NUMBER
+export MYBR=${BUILD_NUMBER}
+if [[ "$((${BUILD_NUMBER}%2))" -eq 1 ]]
+then
+   export MYBR=${BUILD_NUMBER}
+   exit 1
+else
+   exit 0
+fi
